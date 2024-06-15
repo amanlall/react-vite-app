1e84db82615f1646abbffce89c8713112e2fdd10
amanlal786 <contact.amanlall@gmail.com>
2024-06-15 17:43:33 +0530

    background css

--- src/components/customComponents/Hero.tsx
+++ src/components/customComponents/Hero.tsx
@@ -1,76 +1,100 @@
-import './Hero.css'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import './Hero.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { Reveal } from './Reveal';
 import { About } from './About';
 import { Experience } from './Experience';
 
+export const Hero = () => {
+  return (
+    <>
+      <div className="App">
+        <Parallax
+          pages={3}
+          style={{ top: '0', left: '0' }}
+          className="animation"
+        >
+          <ParallaxLayer offset={0} speed={0.5}>
+            <div className="animation_layer parallax" id="artback"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.8}>
+            <div className="animation_layer parallax" id="manonmountain"></div>
+          </ParallaxLayer>
 
-export const Hero = () => { 
-   return (
+          <ParallaxLayer offset={0} speed={0.3}>
+            <div className="animation_layer parallax" id="jungle1"></div>
+          </ParallaxLayer>
 
-<>
-    <div className="App">
+          <ParallaxLayer offset={0} speed={0.35}>
+            <div className="animation_layer parallax" id="jungle2"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.5}>
+            <div className="animation_layer parallax" id="jungle3"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.45}>
+            <div className="animation_layer parallax" id="jungle4"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.8}>
+            <div className="animation_layer parallax" id=""></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.5}>
+            <div className="animation_layer parallax" id="jungle1"></div>
+          </ParallaxLayer>
 
-      <Parallax pages={3} style={{ top: '0', left: '0' }} className="animation">
-            <ParallaxLayer offset={0} speed={0.5}>
-              <div className="animation_layer parallax" id="artback">  
+          <ParallaxLayer offset={1} speed={0.45}>
+            <div className="animation_layer parallax" id="jungle2"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.5}>
+            <div className="animation_layer parallax" id="manonmountain"></div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.45}>
+            <div className="animation_layer parallax" id="artback"></div>
+          </ParallaxLayer>
+          <div>
+            <div className="bg-[#0e101a]">
+              <Reveal
+                children={
+                  <div className="greeting">
+                    <h2 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+                      Hey, I'm Aman
+                    </h2>
+                  </div>
+                }
+              />
+              <Reveal
+                children={
+                  <div className="aboutme font-sans font-bold">
+                    I'm a{' '}
+                    <span className="text-curtainColor">
+                      Full Stack Developer
+                    </span>
+                  </div>
+                }
+              />
+              <Reveal
+                children={
+                  <div className="someinfosection font-sans font-bold">
+                    I've spent the last 5 years building and scaling software
+                    for some pretty cool companies. I also love to discuss ideas
+                    and brainstome solutions!
+                  </div>
+                }
+              />
+              <About />
+              <Reveal
+                children={
+                  <div className="ml-10 text-[#fdfdfd] sm:text-[1rem] md:text-[1rem] lg:text-[2rem] xl:text-3xl font-bold pb-1 font-sans font-bold">
+                    PROJECTS<span className="text-curtainColor">.</span>
+                  </div>
+                }
+              />
+              {/* <div className="flex"><ThreeDCardDemo /></div> */}
+              <div className="flex">
+                <Experience />
+              </div>
             </div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.8}>
-              <div className="animation_layer parallax" id="manonmountain">
-              </div>
-            </ParallaxLayer>
-
-            <ParallaxLayer offset={0} speed={0.3}>
-              <div className="animation_layer parallax" id="jungle1"></div>
-            </ParallaxLayer>
-
-            <ParallaxLayer offset={0} speed={0.35}>
-              <div className="animation_layer parallax" id="jungle2"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.5}>
-              <div className="animation_layer parallax" id="jungle3"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.45}>
-              <div className="animation_layer parallax" id="jungle4"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.8}>
-              <div className="animation_layer parallax" id=""></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={0.5}>
-              <div className="animation_layer parallax" id="jungle1"></div>
-            </ParallaxLayer>
-            
-            <ParallaxLayer offset={1} speed={0.45}>
-              <div className="animation_layer parallax" id="jungle2"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={0.5}>
-              <div className="animation_layer parallax" id="manonmountain"></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed={0.45}>
-              <div className="animation_layer parallax" id="artback"></div>
-            </ParallaxLayer>
-            <div >
-
-              <div className='bg-[#0e101a]'>
-              <Reveal children={<div className="greeting"><h2 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-        Hey, I'm Aman</h2></div>}/>
-      <Reveal children={<div className="aboutme font-sans font-bold">I'm a <span className="text-curtainColor">Full Stack Developer</span></div>}/>
-      <Reveal children={<div className="someinfosection font-sans font-bold">I've spent the last 5 years building and scaling software for some pretty cool companies. I also love to discuss ideas and brainstome solutions!</div>}/>
-      <About/>
-      <Reveal children={ <div className="ml-10 text-[#fdfdfd] sm:text-[1rem] md:text-[1rem] lg:text-[2rem] xl:text-3xl font-bold pb-1 font-sans font-bold">PROJECTS<span className='text-curtainColor'>.</span></div>}/>
-      {/* <div className="flex"><ThreeDCardDemo /></div> */}
-      <div className="flex"><Experience/></div>  
-              </div>
-
-    </div>
-
-      </Parallax>
-
-
-    </div>
-
+          </div>
+        </Parallax>
+      </div>
     </>
   );
 };
-
